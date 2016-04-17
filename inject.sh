04e732c8632bccfd8c6af4e99c8af9825083d1d4
shahar <shahar@budgetao.com>
2016-04-17 12:32:17 +0300

    add

--- inject.sh
+++ inject.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 #export GOOGLE_APPLICATION_CREDENTIALS=Dataflow\ Demo-a647d429d25d.json
-
 #for i in {1..20}
 #do
-
-
-mvn compile exec:java -Dexec.mainClass=com.google.cloud.dataflow.examples.complete.game.injector.Injector -Dexec.args="df-demo injected none" &
-
-
+mvn compile exec:java -Dexec.mainClass=com.google.cloud.dataflow.examples.complete.game.injector.Injector -Dexec.args="df-demo injected none"
 #done
 
